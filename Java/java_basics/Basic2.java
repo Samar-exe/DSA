b2d6ba76c53c75b4338609575934639965b2a81c
Samar-exe <samarik2004@gmail.com>
2024-10-24 13:37:19 +0530

    Input a number and print all the factors of that number (use loops).

--- Java/java_basics/Basic2.java
+++ Java/java_basics/Basic2.java
@@ -163,6 +163,17 @@ public class Basic2 {
 
   }
 
+  static void factors() {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter number: ");
+    int num = sc.nextInt();
+    for (int i = 1; i <= num; i++) {
+      if (num % i == 0) {
+        System.out.println(i);
+      }
+    }
+    sc.close();
+  }
   public static void main(String... args) {
   }
 }
